7a9c9cdf4f9457e74de83a278a156de65e983d97
Max Kolomiyets <therealwacs@gmail.com>
2021-06-09 13:52:57 +0300

    Finish base table logic

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -1,6 +1,6 @@
 import {$} from '@core/dom'
 
-export function resizeHandler($root) {
+export function resizeHandler($root, event) {
   const $resizer = $(event.target)
   const $parent = $resizer.closest('[data-type="resizable"]')
   const coords = $parent.getCoords()
